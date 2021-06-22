087081c13420510059cba2d20fba0902062dfa5e
AlehKatsiuba <oleg@form.io>
2021-06-22 14:47:29 +0300

    Updates

--- src/renderer.js
+++
@@ -1,5 +0,0 @@
-import { Formio } from 'formiojs/formio.form.js';
-import './components/ViewerCalendar';
-import ViewerDateTime from './components/DateTime';
-Formio.Components.setComponent('datetime', ViewerDateTime);
-export { Formio };
